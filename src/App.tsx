834b4fe4e2b59f6355783fbcf336f796855d1b0c
shaunmaselela25 <160026940+shaunmaselela25@users.noreply.github.com>
2026-07-14 08:01:20 +0000

    added functionality to fetch API data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
 // @ts-ignore: allow importing untyped JS components
+import { useEffect, useState } from "react";
+// @ts-ignore: allow importing untyped JS components
 import SearchBar from "./components/SearchBar";
 // @ts-ignore: allow importing untyped JS components
 import UserList from "./components/UserList";
 
 function App() {
+  // State to store users fetched from the API
+  const [users, setUsers] = useState([]);
 
-    return (
+  // Fetch users once when the component mounts
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
 
-        <div className="app">
+      const data = await response.json();
 
-            <header>
+      setUsers(data);
+    }
 
-                <h1>User Directory</h1>
+    fetchUsers();
+  }, []);
 
-            </header>
+  // Temporary: Verify data is being fetched
+  console.log(users);
 
-            <SearchBar />
+  return (
+    <div className="app">
+      <header>
+        <h1>User Directory</h1>
+      </header>
 
-            <UserList />
+      <SearchBar />
 
-        </div>
-
-    );
-
+      <UserList users={users} />
+    </div>
+  );
 }
 
 export default App;
